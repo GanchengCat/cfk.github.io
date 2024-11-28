b7c5bf7d907c7056ed493ca912aad5af8067b4f8
GanchengCat <3250315682@qq.com>
2024-11-28 13:42:45 +0800

    Update theme.ts

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -3,17 +3,6 @@ import { hopeTheme } from "vuepress-theme-hope";
 import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
 
-export default {
-  theme: hopeTheme({
-    plugins: {
-      mdEnhance: {
-        footnote: true,
-      },
-    },
-  }),
-};
-
-
 export default hopeTheme({
   hostname: "https://www.meowra.cn",
 
